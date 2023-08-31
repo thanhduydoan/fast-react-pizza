46f19790e70d2cf66723315a8468fe5bbc42bd14
thanhduydoan <83283966+thanhduydoan@users.noreply.github.com>
2023-08-31 14:32:07 +0700

    add build folder

--- src/index.js
+++ src/index.js
@@ -1,17 +1,131 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { StrictMode, useEffect, useState } from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
 
+const pizzaData = [
+    {
+        name: "Focaccia",
+        ingredients: "Bread with italian olive oil and rosemary",
+        price: 6,
+        photoName: "pizzas/focaccia.jpg",
+        soldOut: false,
+    },
+    {
+        name: "Pizza Margherita",
+        ingredients: "Tomato and mozarella",
+        price: 10,
+        photoName: "pizzas/margherita.jpg",
+        soldOut: false,
+    },
+    {
+        name: "Pizza Spinaci",
+        ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+        price: 12,
+        photoName: "pizzas/spinaci.jpg",
+        soldOut: false,
+    },
+    {
+        name: "Pizza Funghi",
+        ingredients: "Tomato, mozarella, mushrooms, and onion",
+        price: 12,
+        photoName: "pizzas/funghi.jpg",
+        soldOut: false,
+    },
+    {
+        name: "Pizza Salamino",
+        ingredients: "Tomato, mozarella, and pepperoni",
+        price: 15,
+        photoName: "pizzas/salamino.jpg",
+        soldOut: true,
+    },
+    {
+        name: "Pizza Prosciutto",
+        ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+        price: 18,
+        photoName: "pizzas/prosciutto.jpg",
+        soldOut: false,
+    },
+];
+
+function App() {
+    return (
+        <div className="container">
+            <Header />
+            <Menu />
+            <Footer />
+        </div>
+    )
+}
+
+function Header() {
+    return <header className="header">
+        <h1>Fast React Pizza Co.</h1>
+    </header>
+}
+
+function Menu() {
+    const pizzas = pizzaData;
+    const numPizza = pizzas.length
+    return <main className="menu">
+        <h2>Our menu</h2>
+        {numPizza > 0 ? (
+            <>
+                <p>Authentic Italian cuisine. 6 creative dishes to choose from. All from our stone oven, all organic, all delicious</p>
+                <ul className="pizzas">
+                    {pizzas.map((pizza) => (<Pizza pizzaObj={pizza} key={pizza.name} />))}
+                </ul>
+            </>
+        ) : (<p>We'are still working on our menu. Please comback later.</p>)}
+    </main>
+}
+
+function Footer() {
+    const [time, setTime] = useState(new Date().toLocaleTimeString());
+    const hour = new Date().getHours();
+    const openHour = 9;
+    const closeHour = 22;
+    const isOpen = hour >= openHour && hour <= closeHour;
+    console.log(isOpen)
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setTime(new Date().toLocaleTimeString())
+        }, 1000)
+        return () => {
+            clearInterval(interval)
+        }
+    }, [])
+    return (
+        <footer className="footer">
+            {isOpen ? (<Order closeHour={closeHour} />
+            ) : (<p>The time is now: {time}</p>)}
+        </footer >
+    )
+}
+
+function Order({ closeHour, openHour }) {
+    return (
+        <div className='order'>
+            <p>
+                We'are open to {openHour} from {closeHour}: 00. Come visit on us or oder online.
+            </p>
+            <button className="btn"> Order</button>
+        </div>
+    )
+}
+
+function Pizza({ pizzaObj }) {
+    return (
+        <li className={`pizza ${pizzaObj.soldOut ? 'sold-out' : ''}`
+        } >
+            <img src={pizzaObj.photoName} alt={pizzaObj.photoName} />
+            <>
+                <h3>{pizzaObj.name}</h3>
+                <p>{pizzaObj.ingredients}</p>
+                <span>{pizzaObj.soldOut ? ('SOLD OUT') : (pizzaObj.price)}</span>
+            </>
+        </li >
+    )
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<StrictMode><App /></StrictMode>)
